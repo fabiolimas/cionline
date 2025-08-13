2c3a2e22900049431713572b3bc2fd83df5d709d
Fábio Lima <fabiolima01@live.com>
2025-08-13 15:15:41 -0300

    ajuste filtro itens

--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Correspondencia Interna</title>
+    <title>CI Online</title>
 
     <!-- Custom fonts for this template-->
     {{-- <link href="{{asset('fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css"> --}}
