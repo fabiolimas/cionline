4d3c3d7afb6899066b05b7bbccd180424399a315
Fábio Lima <fabiolima01@live.com>
2025-08-13 14:18:41 -0300

    ajuste seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
              'name' => 'Fábio Lima',
              'email' => 'fabiolima01@live.com',
              'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+
              'role'=>'admin'
          ]);
 
@@ -28,6 +29,7 @@ class DatabaseSeeder extends Seeder
              'name' => 'Imagem Jacobina',
              'email' => 'jacobina@lojasimagem.com.br',
              'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+             'loja_id'=>1,
              'role'=>'loja'
          ]);
 
@@ -35,6 +37,7 @@ class DatabaseSeeder extends Seeder
              'name' => 'Imagem Capim Grosso',
              'email' => 'capimgrosso@lojasimagem.com.br',
              'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'loja_id'=>2,
              'role'=>'loja'
          ]);
 
@@ -42,12 +45,14 @@ class DatabaseSeeder extends Seeder
              'name' => 'Imagem Senhor do bonfim',
              'email' => 'senhordobonfim@lojasimagem.com.br',
              'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'loja_id'=>3,
              'role'=>'loja'
          ]);
           User::factory()->create([
              'name' => 'Imagem Juazeiro',
              'email' => 'juazeiro@lojasimagem.com.br',
              'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'loja_id'=>4,
              'role'=>'loja'
          ]);
 
@@ -55,6 +60,7 @@ class DatabaseSeeder extends Seeder
              'name' => 'Imagem Petrolina',
              'email' => 'petrolina@lojasimagem.com.br',
              'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'loja_id'=>5,
              'role'=>'loja'
          ]);
 
@@ -62,6 +68,7 @@ class DatabaseSeeder extends Seeder
              'name' => 'Imagem River',
              'email' => 'river@lojasimagem.com.br',
              'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'loja_id'=>6,
              'role'=>'loja'
          ]);
 
@@ -69,6 +76,7 @@ class DatabaseSeeder extends Seeder
              'name' => 'Imagem Escritório',
              'email' => 'suporte@lojasimagem.com.br',
              'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'loja_id'=>7,
              'role'=>'loja'
          ]);
 
