5b2d04d32d2deaf59dc80dacacd0d5ccc6b61543
Fábio Lima <fabiolima01@live.com>
2025-08-20 15:53:18 -0300

    ajust

--- resources/views/painel/correspondencia/enviado.blade.php
+++ resources/views/painel/correspondencia/enviado.blade.php
@@ -100,11 +100,11 @@
 
                                             <td>{{$correspondencia->funcionario_origem}}</td>
                                              <td>{{$correspondencia->funcionario_destinatario}}</td>
-                                               <td>{{ date('Y-m-d H:i', strtotime($correspondencia->created_at)) }}</td>
+                                               <td>{{ date('d-m-Y H:i', strtotime($correspondencia->created_at)) }}</td>
                                         @if($correspondencia->data_recebimento == null)
                                              <td>-</td>
                                         @else
-                                        <td>{{  date('Y-m-d H:i', strtotime($correspondencia->data_recebimento)) }}</td>
+                                        <td>{{  date('d-m-Y H:i', strtotime($correspondencia->data_recebimento)) }}</td>
                                         @endif
                                                               @if($correspondencia->status == 'aberto')
                                                             <td class="text-danger">Aberto</td>
