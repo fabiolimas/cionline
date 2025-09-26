5017f35e9c1bc949d7a5d5806543c97156d2d377
Fábio Lima <fabiolima01@live.com>
2025-09-26 11:32:23 -0300

    correçao envio

--- resources/views/emails/correspondencia.blade.php
+++ resources/views/emails/correspondencia.blade.php
@@ -7,7 +7,7 @@ Você recebeu uma nova correspondência de **{{ $ci->funcionario_origem }}**.
 **Loja Destinatário:** {{ $ci->loja_destinatario }}
 **Data de Envio:** {{ $ci->data_envio->format('d/m/Y H:i') }}
 
-@component('mail::button', ['url'])
+@component('mail::button', ['url' => route('home')])
 Ver Correspondência
 @endcomponent
 
