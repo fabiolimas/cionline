a6ebbb6630f71cbf6a6aaa5685cf00bf108c1db7
Fábio Lima <fabiolima01@live.com>
2025-09-29 11:29:46 -0300

    ajuste enviados e recebidos

--- resources/views/painel/correspondencia/recebido.blade.php
+++ resources/views/painel/correspondencia/recebido.blade.php
@@ -29,119 +29,134 @@
                             <h6 class="m-0 font-weight-bold text-primary">Recebidas</h6>
                         </div>
                         <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                    <thead>
-                                        <tr>
-                                             <th>CI</th>
-                                            <th>Origem</th>
-                                            <th>Destino</th>
-                                            <th>De</th>
-                                            <th>Para</th>
-                                            <th>Data Envio</th>
-                                            <th>Data Receb</th>
-                                            <th>Status</th>
-                                            <th>Ações</th>
-                                        </tr>
-                                    </thead>
+                    <div class="table-responsive">
+                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th>CI</th>
+                                    <th>Origem</th>
+                                    <th>Destino</th>
+                                    <th>De</th>
+                                    <th>Para</th>
+                                    <th>Data Envio</th>
+                                    <th>Data Receb</th>
+                                    <th>Status</th>
+                                    <th>Ações</th>
+                                </tr>
+                            </thead>
 
-                                    <tbody>
-                                        @foreach($correspondencias as $correspondencia)
-                                        <tr class="@if($correspondencia->loja_id == Auth::user()->id) bg-light text-dark @else  @endif">
-                                           <td>{{$correspondencia->id}}</td>
-                                            @switch($correspondencia->loja_origem)
+                            <tbody>
+                                @foreach ($correspondencias as $correspondencia)
+                                    <tr class="@if ($correspondencia->loja_id == Auth::user()->id) bg-light text-dark @else @endif">
+                                        <td>{{ $correspondencia->id }}</td>
+
+                                        @switch($correspondencia->loja_origem)
                                             @case (1)
-                                            <td>Jacobina</td>
+                                                <td>Jacobina</td>
                                             @break
-                                             @case (2)
-                                            <td>Capim Grosso</td>
-                                            @break
-                                             @case (3)
-                                            <td>Senhor do Bonfim</td>
-                                            @break
-                                             @case (4)
-                                            <td>Juazeiro</td>
-                                            @break
-                                             @case (5)
-                                            <td>Petrolina</td>
-                                            @break
-                                             @case (6)
-                                            <td>River</td>
-                                            @break
-                                             @case (7)
-                                            <td>Escritório</td>
-                                            @break
-                                            @endswitch
 
-                                            @switch($correspondencia->loja_destinatario)
+                                            @case (2)
+                                                <td>Capim Grosso</td>
+                                            @break
+
+                                            @case (3)
+                                                <td>Senhor do Bonfim</td>
+                                            @break
+
+                                            @case (4)
+                                                <td>Juazeiro</td>
+                                            @break
+
+                                            @case (5)
+                                                <td>Petrolina</td>
+                                            @break
+
+                                            @case (6)
+                                                <td>River</td>
+                                            @break
+
+                                            @case (7)
+                                                <td>Escritório</td>
+                                            @break
+                                        @endswitch
+
+                                        @switch($correspondencia->loja_destinatario)
                                             @case (1)
-                                            <td>Jacobina</td>
+                                                <td>Jacobina</td>
                                             @break
-                                             @case (2)
-                                            <td>Capim Grosso</td>
+
+                                            @case (2)
+                                                <td>Capim Grosso</td>
                                             @break
-                                             @case (3)
-                                            <td>Senhor do Bonfim</td>
+
+                                            @case (3)
+                                                <td>Senhor do Bonfim</td>
                                             @break
-                                             @case (4)
-                                            <td>Juazeiro</td>
+
+                                            @case (4)
+                                                <td>Juazeiro</td>
                                             @break
-                                             @case (5)
-                                            <td>Petrolina</td>
+
+                                            @case (5)
+                                                <td>Petrolina</td>
                                             @break
-                                             @case (6)
-                                            <td>River</td>
+
+                                            @case (6)
+                                                <td>River</td>
                                             @break
-                                             @case (7)
-                                            <td>Escritório</td>
+
+                                            @case (7)
+                                                <td>Escritório</td>
                                             @break
-                                            @endswitch
+                                        @endswitch
 
 
-                                            <td>{{$correspondencia->funcionario_origem}}</td>
-                                             <td>{{$correspondencia->funcionario_destinatario}}</td>
-                                                     <td>{{ date('d-m-Y H:i', strtotime($correspondencia->created_at)) }}</td>
-                                        @if($correspondencia->data_recebimento == null)
-                                             <td>-</td>
+                                        <td>{{ $correspondencia->funcionario_origem }}</td>
+                                        <td>{{ $correspondencia->funcionario_destinatario }}</td>
+                                        <td>{{ date('d-m-Y H:i', strtotime($correspondencia->created_at)) }}</td>
+                                        @if ($correspondencia->data_recebimento == null)
+                                            <td>-</td>
                                         @else
-                                        <td>{{  date('d-m-Y H:i', strtotime($correspondencia->data_recebimento)) }}</td>
+                                            <td>{{ date('d-m-Y H:i', strtotime($correspondencia->data_recebimento)) }}</td>
                                         @endif
-                                            @if($correspondencia->status == 'aberto')
+                                        @if ($correspondencia->status == 'aberto')
                                             <td class="text-danger">Aberto</td>
-                                                @else
-                                                <td class="text-success">Recebido</td>
-
-                                              @endif
-                                            <td><a href="{{ route('ci', $correspondencia->id) }}" class="btn btn-info"
+                                        @else
+                                            <td class="text-success">Recebido</td>
+                                        @endif
+                                        <td><a href="{{ route('ci', $correspondencia->id) }}" class="btn btn-info"
                                                 title="Visualizar"><i class="fa fa-eye"></i></a>
 
-                                            @if ($correspondencia->loja_id == Auth::user()->id)
-                                            @if($correspondencia->status == 'recebido')
+                                            @if ($correspondencia->loja_origem == Auth::user()->loja_id)
+                                                @if ($correspondencia->status == 'recebido')
+
+                                                @else
+                                                    <a href="{{ route('editar-ci', $correspondencia->id) }}"
+                                                        class="btn btn-success" title="Editar"><i
+                                                            class="fa fa-pencil"></i></a>
+                                                    <a href="{{ route('delete', $correspondencia->id) }}"
+                                                        class="btn btn-danger" title="Excluir"><i
+                                                            class="fa fa-trash"></i></a>
+                                                @endif
+                                            @elseif(Auth::user()->loja_id == null)
 
                                             @else
+                                                @if ($correspondencia->status == 'recebido')
 
-                                             <a href="{{route('editar-ci', $correspondencia->id)}}" class="btn btn-success" title="Editar"><i
-                                                        class="fa fa-pencil"></i></a>
-                                            <a href="{{route('delete', $correspondencia->id)}}" class="btn btn-danger" title="Excluir"><i
-                                                        class="fa fa-trash"></i></a>
+                                                @else
+                                                    <a href="{{ route('confirma', $correspondencia->id) }}"
+                                                        class="btn btn-danger" title="Confirmar"><i
+                                                            class="fa fa-check"></i></a>
                                             @endif
-                                            @else
-                                               @if($correspondencia->status == 'recebido')
-
-                                               @else
-                                                <a href="{{route('confirma', $correspondencia->id)}}" class="btn btn-danger" title="Confirmar"><i
-                                                        class="fa fa-check"></i></a>
-                                                @endif
                                         </td>
-                                                @endif
-                                        </tr>
+                                @endif
+                                </tr>
+                                @endforeach
 
-                                        @endforeach
-
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
                     </div>
                         </div>
 
