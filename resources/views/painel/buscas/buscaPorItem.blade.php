d32966650cba00b730d99f6423930404e3e0dd3d
Fábio Lima <fabiolima01@live.com>
2025-10-10 14:03:43 -0300

    formataçao data buscas

--- resources/views/painel/buscas/buscaPorItem.blade.php
+++ resources/views/painel/buscas/buscaPorItem.blade.php
@@ -85,7 +85,10 @@
                                         <td>{{ $correspondencia->funcionario_origem }}</td>
                                         <td>{{ $correspondencia->funcionario_destinatario }}</td>
                                         <td>{{ date('d-m-Y H:i', strtotime($correspondencia->data_envio)) }}</td>
-                                        <td>{{ date('d-m-Y H:i', strtotime($correspondencia->data_recebimento)) }}</td>
+                                        <td> - </td>
+                    @else
+                    <td>{{ date('d-m-Y H:i', strtotime($correspondencia->data_recebimento)) }}</td>
+                    @endif
                                         @if($correspondencia->status == 'aberto')
                                             <td class="text-danger">Aberto</td>
                                         @else
