2d7c983a2b737b15c12e78af3054a2418534dfe9
Fábio Lima <fabiolima01@live.com>
2025-10-10 14:02:08 -0300

    ajuste data buscas

--- resources/views/painel/buscas/buscaPorLoja.blade.php
+++ resources/views/painel/buscas/buscaPorLoja.blade.php
@@ -82,8 +82,8 @@
 
                     <td>{{ $correspondencia->funcionario_origem }}</td>
                     <td>{{ $correspondencia->funcionario_destinatario }}</td>
-                    <td>{{ $correspondencia->data_envio }}</td>
-                    <td>{{ $correspondencia->data_recebimento }}</td>
+                    <td>{{ date('d-m-Y H:i', strtotime($correspondencia->data_envio)) }}</td>
+                    <td>{{ date('d-m-Y H:i', strtotime($correspondencia->data_recebimento)) }}</td>
                     @if ($correspondencia->status == 'aberto')
                         <td class="text-danger">Aberto</td>
                     @else
