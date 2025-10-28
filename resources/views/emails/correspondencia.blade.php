d2ca04feaf872f50580a76a09e98347d5dddec01
Fábio Lima <fabiolima01@live.com>
2025-10-28 10:53:03 -0300

    ajuste link email

--- resources/views/emails/correspondencia.blade.php
+++ resources/views/emails/correspondencia.blade.php
@@ -5,7 +5,7 @@ Você recebeu uma nova correspondência de **{{ $ci->funcionario_origem }}**.
 
 **Data de Envio:** {{ $ci->data_envio->format('d/m/Y H:i') }}
 
-@component('mail::button', ['url' => '/'])
+@component('mail::button', ['url' => 'https://cionline.lojasimagem.com.br/'])
 Ver Correspondência
 @endcomponent
 
