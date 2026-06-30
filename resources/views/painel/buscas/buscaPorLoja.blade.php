78706cecd0a834031a348f48b18470990f95e899
Fábio Lima <fabiolima01@live.com>
2026-06-30 09:18:48 -0300

    exclusão na busca adminsitrador

--- resources/views/painel/buscas/buscaPorLoja.blade.php
+++ resources/views/painel/buscas/buscaPorLoja.blade.php
@@ -97,7 +97,7 @@
                     <td><a href="{{ route('ci', $correspondencia->id) }}" class="btn btn-info" title="Visualizar"><i
                                 class="fa fa-eye"></i></a>
 
-                        @if ($correspondencia->loja_origem == Auth::user()->loja_id)
+                        @if ($correspondencia->loja_origem == Auth::user()->loja_id || Auth::user()->role == 'admin')
                             @if ($correspondencia->status == 'recebido')
                             @else
                                 <a href="{{ route('editar-ci', $correspondencia->id) }}" class="btn btn-success"
